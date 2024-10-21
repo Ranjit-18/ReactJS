4ba7c43c7c8f6a2b907d97915ed9df2438a70709
Ranjit-18 <140911978+Ranjit-18@users.noreply.github.com>
2024-10-21 22:27:26 +0530

    fun1

---
+++ increment-count/src/App.jsx
@@ -0,0 +1,18 @@
+import Apples from "./Apples";
+import IncDec from "./IncDec";
+import Increament from "./Increament";
+import ScoreCard from "./ScoreCard"
+function App()
+{
+  return(
+    <>
+    
+      <Increament />
+      <IncDec />
+      <ScoreCard />
+      <Apples />
+    </>
+  )
+}
+
+export default App;
